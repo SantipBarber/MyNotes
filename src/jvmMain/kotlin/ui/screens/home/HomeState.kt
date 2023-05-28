3a8115332f66b9a3644a00939e82a7caba2eb118
SpBarber <santipbr@gmail.com>
2023-05-28 19:49:21 +0200

    Se crea un extension property

--- src/jvmMain/kotlin/ui/screens/home/HomeState.kt
+++ src/jvmMain/kotlin/ui/screens/home/HomeState.kt
@@ -1,31 +1,48 @@
 package ui.screens.home
 
 import data.model.Filter
+import data.model.Note
+import data.model.fakeNotes
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import data.model.Note
-import data.model.getNotes
-import kotlinx.coroutines.flow.update
+import kotlin.reflect.KProperty
+
+operator fun <T> StateFlow<T>.getValue(
+    owner: Any?,
+    property: KProperty<*>
+): T = this.value
+
+operator fun <T> MutableStateFlow<T>.setValue(
+    owner: Any?,
+    property: KProperty<*>,
+    newValue: T
+) {
+    this.value = newValue
+}
+
 
 object HomeState {
-    private val _state = MutableStateFlow(UiState())
-    val state = _state.asStateFlow()
+    //private val _state = MutableStateFlow(UiState())
+    // val state = _state.asStateFlow()
+
+    var state: UiState by MutableStateFlow(UiState())
+        private set
     fun loadNotes(coroutineScope: CoroutineScope) {
         coroutineScope.launch {
-            _state.value = UiState(isLoading = true)
+            state = UiState(isLoading = true)
 
 
-            getNotes().collect {
-                _state.value = UiState(isLoading = false, notes = it)
+            Note.fakeNotes.collect {
+                state = UiState(isLoading = false, notes = it)
             }
         }
     }
 
 
     fun onFilterClick(filter: Filter) {
-        _state.update { it.copy(filter = filter) }
+        state = state.copy(filter = filter)
     }
 
     data class UiState(
